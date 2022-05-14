4da14d2d60ad4aca4ee012cab8ad6f661650c74f
Rhayonne <rhayonne@gmail.com>
2022-05-14 23:56:04 +0200

    je besoin dinnergit add .git add .

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80;
+console.log(`Votre vitesse est ${vel}Km/h`);
+if (vel > 60){
+    console.log(`Vous vennez de se faire flasher`);
+}
+console.log(`Securité sur tout`);
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pays = 'USA';
+if (pays == 'BR') {
+    console.log(`Vous vivrez au brésilien`);
+    
+}else{
+    console.log(`vous vivrez aux ${pays}`);
+}
